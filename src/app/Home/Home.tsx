eb67a2bb443fa41ee403873e32265bfb355df688
Kevin Amiranoff <kamiranoff@gmail.com>
2017-12-28 16:43:44 +0000

    change to class component

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Component } from 'react';
 
 const Title = styled.h1`
   font-size: 2rem;
@@ -10,9 +11,12 @@ const Header = styled.header`
   background: #D4C635;
   color: #2D3DA0;
   padding: 25px;
- 
 `;
 
-const Home = () => (<Header><Title>X-Men</Title></Header>);
+class Home extends Component {
+  public render() {
+    return (<Header><Title>X-Men</Title></Header>);
+  }
+}
 
 export default Home;
